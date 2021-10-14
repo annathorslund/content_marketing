c9b7b75a43a4700707e896343a0787099cd6d4f0
rona0515 <rona0515@stud.kea.dk>
2021-10-14 13:08:55 +0200

    animation på sectioner - klaret

--- index.js
+++ index.js
@@ -1,14 +1,100 @@
-gsap.registerPlugin(ScrollTrigger);
+// -------Animation for pSection----- //
+// gsap.to(".pContent", {
+//   yPercent: -100,
+//   ease: "none",
+//   scrollTrigger: {
+//     trigger: ".pSection",
+//     // start: "top bottom", // the default values
+//     // end: "bottom top",
+//     scrub: true,
+//     markers: true,
+//   },
+// });
 
-gsap.to(".section-02 h2", {
+// gsap.to(".pImage", {
+//   yPercent: 50,
+//   ease: "none",
+//   scrollTrigger: {
+//     trigger: ".pSection",
+//     // start: "top bottom", // the default values
+//     // end: "bottom top",
+//     scrub: true,
+//   },
+// });
+
+// -------Animation for Section 1----- //
+
+gsap.to(".animation_section_content1", {
+  yPercent: 10,
+  ease: "none",
   scrollTrigger: {
-    scroller: ".panel",
-    trigger: ".section_hayde",
-    start: "center 55%",
+    trigger: ".animation_section1",
+    // start: "top bottom", // the default values
+    // end: "bottom top",
+    scrub: true,
     markers: true,
-    toggleActions: "play complete restart reverse",
   },
-  scale: 4,
-  duration: 2,
-  ease: "back",
+});
+
+gsap.to(".animation_section_image1", {
+  yPercent: -60,
+  ease: "none",
+  scrollTrigger: {
+    trigger: ".animation_section1",
+    // start: "top bottom", // the default values
+    // end: "bottom top",
+    scrub: true,
+  },
+});
+
+// -------Animation for Section 2----- //
+
+gsap.to(".animation_section_content2", {
+  yPercent: -30,
+  ease: "none",
+  scrollTrigger: {
+    trigger: ".animation_section2",
+    // start: "top bottom", // the default values
+    // end: "bottom top",
+    scrub: true,
+    markers: true,
+  },
+});
+
+gsap.to(".animation_section_image2", {
+  yPercent: -50,
+  ease: "none",
+  scrollTrigger: {
+    trigger: ".animation_section2",
+    // start: "top bottom", // the default values
+    // end: "bottom top",
+    scrub: true,
+  },
+});
+
+// -------Animation for Section 2.5----- //
+
+// -------Animation for Section 4----- //
+
+gsap.to(".animation_section_content4", {
+  yPercent: 30,
+  ease: "none",
+  scrollTrigger: {
+    trigger: ".animation_section4",
+    // start: "top bottom", // the default values
+    // end: "bottom top",
+    scrub: true,
+    markers: true,
+  },
+});
+
+gsap.to(".animation_section_image4", {
+  yPercent: -50,
+  ease: "none",
+  scrollTrigger: {
+    trigger: ".animation_section4",
+    // start: "top bottom", // the default values
+    // end: "bottom top",
+    scrub: true,
+  },
 });
