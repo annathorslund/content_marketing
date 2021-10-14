aae207410a4d41bf330ec78df3d8ed3e2f45f14b
Anna Thorslund <anna728x@stud.kea.dk>
2021-10-14 13:13:48 +0200

    y

--- index.js
+++ index.js
@@ -12,3 +12,18 @@ gsap.to(".section-02 h2", {
   duration: 2,
   ease: "back",
 });
+
+gsap.to("#nudel_sprite", {
+  rotate: 360,
+  duration: 1.5,
+  ease: "linaer",
+  scrollTrigger: {
+    trigger: "#nudel_sprite",
+    start: "top 65%",
+    toggleActions: "play reverse",
+    markers: true,
+  },
+});
+
+
+
